54574612814efdf2bd99b3e3778f89ec2b13f7bf
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-05-03 20:42:59 +0300

    commit

--- src/main/java/com/daniil1380/tinder/service/impl/HighestScoreMatchService.java
+++ src/main/java/com/daniil1380/tinder/service/impl/HighestScoreMatchService.java
@@ -2,11 +2,14 @@ package com.daniil1380.tinder.service.impl;
 
 import com.daniil1380.tinder.entity.User;
 import com.daniil1380.tinder.service.MatchService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HighestScoreMatchService implements MatchService {
 
     @Override
     public User getNewMatch() {
+        System.out.println("нашелся самый популярный человек");
         return null;
     }
 }
--- src/main/java/com/daniil1380/tinder/service/impl/RandomMatchService.java
+++ src/main/java/com/daniil1380/tinder/service/impl/RandomMatchService.java
@@ -2,11 +2,14 @@ package com.daniil1380.tinder.service.impl;
 
 import com.daniil1380.tinder.entity.User;
 import com.daniil1380.tinder.service.MatchService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RandomMatchService implements MatchService {
 
     @Override
     public User getNewMatch() {
+        System.out.println("Нашелся случайный человек");
         return null;
     }
 
