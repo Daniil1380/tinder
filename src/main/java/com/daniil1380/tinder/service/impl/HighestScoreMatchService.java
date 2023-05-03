ad16d194fa1b7434e2887fafb3bfe3637f173162
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-05-03 22:13:32 +0300

    commit

--- src/main/java/com/daniil1380/tinder/service/impl/HighestScoreMatchService.java
+++ src/main/java/com/daniil1380/tinder/service/impl/HighestScoreMatchService.java
@@ -6,15 +6,30 @@ import com.daniil1380.tinder.service.MatchService;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@Primary
 public class HighestScoreMatchService implements MatchService {
 
     private UsersRepository usersRepository;
 
+    public HighestScoreMatchService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
     @Override
     public User getNewMatch() {
-        System.out.println("HighestScoreMatchService");
-        return null;
+        List<User> users = usersRepository.getUserList();
+        int maxPoint = 0;
+        User highestRangUser = null;
+
+        for (User user : users) {
+            if (user.getPoints() > maxPoint) {
+                maxPoint = user.getPoints();
+                highestRangUser = user;
+            }
+        }
+
+        return highestRangUser;
     }
 }
--- src/main/java/com/daniil1380/tinder/service/impl/RandomMatchService.java
+++ src/main/java/com/daniil1380/tinder/service/impl/RandomMatchService.java
@@ -3,17 +3,28 @@ package com.daniil1380.tinder.service.impl;
 import com.daniil1380.tinder.entity.User;
 import com.daniil1380.tinder.repository.UsersRepository;
 import com.daniil1380.tinder.service.MatchService;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Random;
+
 @Service
+@Primary
 public class RandomMatchService implements MatchService {
 
     private UsersRepository usersRepository;
 
+    public RandomMatchService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
     @Override
     public User getNewMatch() {
-        System.out.println("RandomMatchService");
-        return null;
+        List<User> users = usersRepository.getUserList();
+        Random random = new Random();
+        int i = random.nextInt(users.size());
+        return users.get(i);
     }
 
 }
