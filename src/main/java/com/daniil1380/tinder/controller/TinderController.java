4a992a420dd0efc57460d0eb5b4e13787c2868b2
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-05-03 20:59:02 +0300

    commit

--- src/main/java/com/daniil1380/tinder/controller/TinderController.java
+++ src/main/java/com/daniil1380/tinder/controller/TinderController.java
@@ -1,15 +1,24 @@
 package com.daniil1380.tinder.controller;
 
 import com.daniil1380.tinder.entity.User;
+import com.daniil1380.tinder.service.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TinderController {
 
+    private MatchService matchService;
+
+    @Autowired
+    public TinderController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
     @GetMapping(value = "/new-match")
     public User getNewMatch() {
-
+        return matchService.getNewMatch();
     }
 
 }
