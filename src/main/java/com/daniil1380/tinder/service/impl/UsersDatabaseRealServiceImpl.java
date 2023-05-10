140974e7941aae3d9da797bcd3c40c913a6ce3ec
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-05-10 21:39:09 +0300

    commit

---
+++ src/main/java/com/daniil1380/tinder/service/impl/UsersDatabaseRealServiceImpl.java
@@ -0,0 +1,14 @@
+package com.daniil1380.tinder.service.impl;
+
+import com.daniil1380.tinder.entity.User;
+import com.daniil1380.tinder.service.UsersDatabaseService;
+
+import java.util.List;
+
+public class UsersDatabaseRealServiceImpl implements UsersDatabaseService {
+
+    @Override
+    public List<User> getUserList() {
+        return null;
+    }
+}
