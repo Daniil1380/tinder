613d43294a150f3ccd2edd08ca20436cb9dc9bfc
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-05-10 21:29:46 +0300

    commit

--- src/main/java/com/daniil1380/tinder/entity/User.java
+++ src/main/java/com/daniil1380/tinder/entity/User.java
@@ -1,14 +1,22 @@
 package com.daniil1380.tinder.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "sex")
     private Sex sex;
 
+    @Column(name = "points")
     private int points;
 
 }
